eccc939e76eb9a7d5bc7531d0967a9667d3b6903
Marton Magai <magai.marton@gmail.com>
2021-10-24 20:59:23 +0200

    Stuff

---
+++ generate_groups.py
@@ -0,0 +1,212 @@
+import os.path
+import random
+
+def check_file_name(file):
+    return os.path.isfile(file)
+
+
+def read_data_file(file='input.txt'):
+    with open(file, 'r') as f:
+        previous_groups = f.readlines()
+        return previous_groups
+
+
+def export_possible_groups(possible_goups, output=['output.txt']):
+    if output != ["not possible to form groups"]:
+        for i in range(len(output)):
+            with open(output[i], 'w') as f:
+                possible_goups[i].append('\n')
+                possible_goups[i] = ', '.join(possible_goups[i])
+                f.write(possible_goups[i])
+                # all_groups = []
+                # for group in possible_goups:
+                #     group = ', '.join(group)
+                #     all_groups.append(group)
+                # f.write('\n'.join(all_groups))
+    else:
+        print("Not possible to form groups!")
+
+
+# TODO should rename some variable to make it more readable!
+def get_formed_prev_groups(previous_groups):
+    formed_prev_groups = []
+    prev_formed_groups = []
+    for i in range(len(previous_groups)):
+        formed_prev_groups = previous_groups[i].split('\n,')
+        formed_prev_groups = formed_prev_groups[-1].strip('\n')
+        formed_prev_groups = formed_prev_groups.split(', ')
+        prev_formed_groups.append(formed_prev_groups)
+    return prev_formed_groups
+
+
+def have_been_together(formed_prev_groups):
+    every_one = []
+    have_been_together_with = {}
+    for i in range(len(formed_prev_groups)):
+        for j in range(len(formed_prev_groups[i])):
+            if formed_prev_groups[i][j] not in every_one:
+                every_one.append(formed_prev_groups[i][j])
+                have_been_together_with[formed_prev_groups[i][j]] = []
+    for i in range(len(formed_prev_groups)):
+        for name in have_been_together_with:
+            if name in formed_prev_groups[i]:
+                for j in range(len(formed_prev_groups[i])):
+                    if formed_prev_groups[i][j] not in have_been_together_with[name]:
+                        have_been_together_with[name].append(formed_prev_groups[i][j])
+    return have_been_together_with, every_one
+
+
+def get_unpaird_people(have_been_together_with, every_one):
+    not_been_togheter = {}
+    for name in have_been_together_with:
+        not_been_togheter[name] = []
+    for person in every_one:
+        for name in have_been_together_with:
+            for _ in have_been_together_with[name]:
+                if person not in have_been_together_with[name]:
+                    if person not in not_been_togheter[name]:
+                        not_been_togheter[name].append(person)
+    return not_been_togheter
+
+
+# TODO fix this crap.
+def get_possible_goups(unpaird_people, group_size):
+    possible_goups = []
+    never_been_together = []
+    for person in unpaird_people:
+        possible_goup = []
+        if unpaird_people[person] != []:
+            if person not in possible_goup:
+                possible_goup.append(person)
+                if len(possible_goup) <= group_size:
+                    for i in range(len(unpaird_people[person])):
+                        if unpaird_people[person][i] not in possible_goup:
+                            possible_goup.append(unpaird_people[person][i])
+                    # all_group_comp = []
+                    # for i in range(len(possible_goup)):
+                    #     current_comp = [possible_goup[i]]
+                    #     for j in range(len(possible_goup)):
+                    #         if possible_goup[j] != possible_goup[i]:
+
+                    #             while len(current_comp) < group_size:
+                    #                 if possible_goup[j] not in current_comp:
+                    #                     current_comp.append(possible_goup[j])
+                    #                 all_group_comp.append(possible_goup[j])
+
+        if possible_goup != []:
+            if never_been_together == []:
+                never_been_together.append(sorted(possible_goup))
+            else:
+                for i in range(len(never_been_together)):
+                    if sorted(possible_goup) not in never_been_together:
+                        never_been_together.append(sorted(possible_goup))
+    # for person in unpaird_people:
+    #     possible_goup = []
+    #     if unpaird_people[person] != []:
+    #         possible_goup.append(person)
+    #         group_mate = []
+            # for 
+    for group in never_been_together:
+        if len(group) == group_size:
+            possible_goups.append(group)
+    return possible_goups
+
+
+def export_file_names(possible_goups):
+    output = []
+    amount_of_files = len(possible_goups)
+    if amount_of_files == 1:
+        return ["output.txt"]
+    elif amount_of_files == 0:
+        return ["not possible to form groups"]
+    else:
+        for i in range(amount_of_files):
+            output.append(f"output_{i + 1}.txt")
+        return output
+
+
+def read_and_get_prev_group_comps(file='input.txt'):
+    previous_groups = read_data_file(file)
+    prev_formed_groups = get_formed_prev_groups(previous_groups)
+    have_been_together_with, every_one = have_been_together(prev_formed_groups)
+    return have_been_together_with, every_one
+
+
+def generate_groups(file, group_size):
+    have_been_together_with, every_one = read_and_get_prev_group_comps(file)
+    unpaird_people = get_unpaird_people(have_been_together_with, every_one)
+    possible_goups = get_possible_goups(unpaird_people, group_size)
+    return possible_goups
+
+
+def get_group_size():
+    while True:
+        group_size = input('How big is a team?\n')
+        if group_size.isnumeric():
+            if int(group_size) > 1:
+                break
+    return int(group_size)
+
+
+def main():
+    # file = 'test_.txt'
+    file = 'input.txt'
+    if check_file_name(file):
+        group_size = get_group_size()
+        possible_goups = generate_groups(file, group_size)
+        output = export_file_names(possible_goups)
+        export_possible_groups(possible_goups, output)
+    else:
+        print('Invalid ')
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+""" Kombinatorikai képlettel randomizáló módszerrel kiválasztja az összes ismétlés nélküli kombinációt egy listából """
+
+
+# from os import name
+# import random
+# import math
+
+
+# group_size = 5
+
+# names = ['Jhon', 'Thomas', 'Peter', 'Jennifer']
+
+# dict = {'Jhon': ['Thomas', 'Peter', 'Jennifer'],
+#         'Marta': ['Peter', 'Jennifer'],
+#         'Liza': ['Thomas', 'Peter', 'Jennifer'],
+#         'Thomas': ['Jhon', 'Liza', 'Jennifer'],
+#         'Peter': ['Jhon', 'Marta', 'Liza', 'Jennifer'],
+#         'Jennifer': ['Jhon', 'Marta', 'Liza', 'Thomas', 'Peter']}
+
+# possible_num = math.factorial(len(names)) / (math.factorial(group_size) * math.factorial(len(names) - group_size))      #  n! / (k! * (n-k)!)
+# print(possible_num)
+# possible_groups = []
+
+# while len(possible_groups) != possible_num:
+#     temp = []
+#     for i in range(group_size):
+#         while True:
+#             rand = random.choice(names)
+#             if rand not in temp:
+#                 break
+#         temp.append(rand)
+#     if sorted(temp) not in possible_groups:
+#         possible_groups.append(sorted(temp))
+#     # print(temp)
+
+# print(possible_groups)
+
+# final = []
+# print(names[0])
+# for group in possible_groups:
+#     if sorted(group) not in final and names[0] in group:
+#         final.append(sorted(group))
+
+# print(final)
---
+++ readme.txt
@@ -0,0 +1,37 @@
+## Task:
+Write a Python program which is able to generate groups from students.
+
+## Requirements
+1. Program shall work with different group sizes, size of the group shall be requested from the user as an terminal input. 
+1. Program shall generate optimal groups, based on the following logic:
+student who participated previously with another student, try not to put them into the same group
+1. Program shall read the input file (input.txt), where students are listed in the same line means they had same group previously, e.g.:
+Jhon, Marta, Liza
+Marta, Thomas
+Peter, Thomas
+Jennifer
+means (Jhon, Marta and Liza) and (Marta, Thomas) and (Peter, Thomas) had a common group and Jennifer had no group previously.
+1. Program shall generate output (output.txt), where the optimal groups are listed, e.g.:
+Thomas, Jhon
+Marta, Peter
+Liza, Jennifer
+1. In case of multiple optimal groups, the program shall generate multiple output files (output_1.txt, output_2.txt, ...) containing each optimal group.
+1. In case of no solution, the program shall generate an output file (output.txt) and write to the first line: "not possible to form groups"
+
+## Deadline
+Week A: 2021.10.24, end of day (23:59)
+Week B: 2021.10.31, end of day (23:59)
+
+## Soulution
+Soultion shall be available in a github repository, and shall be presented the logic on 2021.10.25. Soultion will be tested with a module test, based on the requirements.
+
+## Winners
+Multiple winners are possible, and get a CHEAT CARD for the ProgBasics PA; in case of any obstacle in the PA, winner can ask a specific question and mentor will provide support. (not possible to ask not specific questions, e.g.: 'what is this', 'why not working', etc.)
+
+Are you interested in?
+
+======================================================================
+
+UPDATE!
+I will provide a :python:  code skeleton :python: soon, please use this for your implementation! 
+
